7f280087e68d5947d0f41a3a94004ab50bf4cfc0
Pooja <poojarajput9516@gmail.com>
2023-03-30 10:41:35 +0530

    user module nd controller functionalty

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-const server = express();
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({extended:true}));
-server.use("/admin",AdminRoutes);
-server.use("/user",UserRoutes);
-server.use("/",RouteRoutes);
-server.listen(3000,()=>console.log("Project Server Started..."));
+import UserRoutes from "./routes/user.route.js"
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use("/admin",AdminRoutes);
+app.use("/user",UserRoutes);
+app.use("/",RouteRoutes);
+app.listen(3000,()=>console.log("Project Server Started..."));
